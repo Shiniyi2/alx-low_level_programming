f138543ed444a6123b027e01c36efec97d19e954
yahya Fatai <yahyafatai@gmail.com>
2022-09-10 20:06:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-/**
- * main-entry point for the program.
- *
- * Return:0 if no errors, return non zero if errors
- */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+
+ char c;
+
+ int i;	
+ long ii;
+ long long iii;	
+ float f;
+ 
+ printf("Size of a char: %ld byte(s)\n", sizeof(c));
+
+ printf("Size of an int: %ld byte(s)\n", sizeof(i));
+ 
+ printf("Size of a long int: %ld byte(s)\n", sizeof(ii)); 		
+ 
+ printf("Size of a long long int: %ld byte(s)\n", sizeof(iii));
+										 printf("Size of a float: %ld byte(s)\n", sizeof(f));
+								 		  return (0);
+
 }
