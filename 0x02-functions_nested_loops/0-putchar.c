a4aa2f9a2c5853682d8e48e663b9134e638975f1
yahya Fatai <yahyafatai@gmail.com>
2022-09-13 17:51:49 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,22 @@
-#include <stdio.h>
-/* A program that prints _putchar, followed by a new line.*/
+#include "main.h"
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
 int main(void)
-{	/* The code to print _putchar */
-	printf("_putchar \n");
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
-}
 
+}
