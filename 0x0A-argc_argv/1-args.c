8980d8fc5d5af8b38c3140d885d4803c3caf4151
yahya Fatai <yahyafatai@gmail.com>
2022-09-30 20:36:43 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+/**
+ * main - print the name number of argument passe to it
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointer to strings
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[] __attribute__((unuse))
+{
+	printf("%d\n",argc -1);
+	return(0);
+}
