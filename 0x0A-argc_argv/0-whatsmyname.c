b9ed2a4883e6cba334fb1ac2584cb41e9616c51a
yahya Fatai <yahyafatai@gmail.com>
2022-09-30 19:22:40 -0700

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main-> this is a functionto print its name 
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ * /
+ * 
+int main(int argc_attribute_((unused)), char *agrv[])
+{
+    printf("%s\n", *argv);
+    return 0;
+}
