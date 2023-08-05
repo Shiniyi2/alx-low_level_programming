5af1e432abfebd55966a6dadd7616e3dd4dbe1b7
Your shiniyi2 <yahyafatai@gmail.com>
2023-08-05 00:02:41 -0700

    password

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- * get_endianness - checks whether a machine is big endian or little                                                             *
- * Return: 1 if little endian, 0 otherwise                       */                                                             int get_endianness(void)
-{                                                                       unsigned int test = 1;
-        char *endian = (char *)&test;
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x;
+	char *c;
 
-        if (*endian)
-                return (1);
-        return (0);
-}
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
+}}
